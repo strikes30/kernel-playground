bcd4c0b7671917dbd59591e21b08c36ec5c176ae
Andrea Mayer <andrea.mayer@uniroma2.it>
2024-04-24 16:37:23 +0000

    WIP add timer support to packet rate algo
    Signed-off-by: Andrea Mayer <andrea.mayer@uniroma2.it>

--- src/c/tc.bpf.c
+++ src/c/tc.bpf.c
@@ -115,6 +115,8 @@ static __always_inline void __write_once_size(volatile void *p, void *res, int s
 	__u.__val;					\
 })
 
+#define skb_data(addr)	((void *)(__u64)(addr))
+
 /* in KB */
 #define RB_SIZE (254 * 1024)
 struct {
@@ -122,6 +124,7 @@ struct {
 	__uint(max_entries, RB_SIZE);
 } rbuf SEC(".maps");
 
+#if 0
 struct hmap_elem_ab {
 	__u64 a;
 	__u64 b;
@@ -333,143 +336,6 @@ lookup:
 	return val;
 }
 
-#define skb_data(addr)	((void *)(__u64)(addr))
-
-
-#define LOG_SCALE_FACTOR	20
-
-#define DECAY_TABLE_MAX		10
-static const __u64 decay_table[DECAY_TABLE_MAX + 1] = {
-	1048576,
-	635993,
-	385750,
-	233969,
-	141909,
-	86072,
-	52206,
-	31664,
-	19205,
-	11649,
-	7065,
-};
-
-#define SWIN_SCALER		1000000000l /* 1sec in nanosec */
-struct slotted_window {
-	/* avoid multiple concurrent window updates */
-	__u64 sync;
-
-	__u64 tsw;
-	__u64 cnt;
-	__u64 avg;
-};
-
-struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__uint(max_entries, 1);
-	__type(key, __u32);
-	__type(value, struct slotted_window);
-} hmapsw SEC(".maps");
-
-#define __LOG_SCALE_IN(x)	(((__u64)(x)) << LOG_SCALE_FACTOR)
-#define __LOG_SCALE_OUT(x)	(((__u64)(x)) >> LOG_SCALE_FACTOR)
-
-#define try_swin_lock(sw)	__sync_lock_test_and_set(&(sw)->sync, 1)
-#define swin_unlock(sw)					\
-	do {						\
-		__sync_fetch_and_and(&(sw)->sync, 0);	\
-		barrier();				\
-	} while(0)
-
-static __always_inline int update_window(struct slotted_window *sw, __u64 ts)
-{
-	const __u64 cur_tsw = ts / SWIN_SCALER;
-	__u64 tsw = READ_ONCE(sw->tsw);
-	__u64 *cnt = &sw->cnt;
-	__u64 cnt_val;
-	__u64 delta;
-	__u64 avg;
-
-	if (cur_tsw <= tsw)
-		goto update;
-
-	if (try_swin_lock(sw))
-		/* busy, another cpu is currently closing the window */
-		goto update;
-
-	/* the current window must be closed */
-	delta = cur_tsw - tsw;
-	if (delta <= DECAY_TABLE_MAX) {
-		if (delta < 1)
-			/* impossibile, uhm? */
-			goto err;
-
-		cnt_val = READ_ONCE(*cnt);
-
-		avg = __LOG_SCALE_OUT((__LOG_SCALE_IN(cnt_val) *
-				      decay_table[(delta - 1)])) +
-			/* sw->avg is only read in this section */
-		      __LOG_SCALE_OUT(sw->avg * decay_table[delta]);
-	} else {
-		avg = 0;
-	}
-
-	WRITE_ONCE(sw->avg, avg);
-
-	/* TODO: send to RING BUFFER rather than print on trace */
-	bpf_printk("CLOSING window; tsw=%llu, avg(fp)=%llu, avg=%llu",
-		   tsw, avg, __LOG_SCALE_OUT(avg));
-
-	/* time to create a new window */
-	WRITE_ONCE(*cnt, 0);
-	WRITE_ONCE(sw->tsw, cur_tsw);
-
-	/* we cannot rely upon the __sync_lock_release() semantic, so we need
-	 * to use a workaround, e.g.: manually set the sw->sync back to 0.
-	 */
-	swin_unlock(sw);
-
-update:
-	cnt_val = __sync_fetch_and_add(cnt, 1);
-
-	bpf_printk("sw ts=%llu, cnt=%llu", ts, cnt_val);
-
-err:
-	/* bug */
-	return -EINVAL;
-}
-
-static __always_inline void eval_avg()
-{
-	__u64 ts = bpf_ktime_get_boot_ns();
-	struct slotted_window *sw;
-	const __u32 key = 0;
-	int rc;
-
-	sw = bpf_map_lookup_elem(&hmapsw, &key);
-	if (!sw) {
-		struct slotted_window _sw;
-
-		memset(&_sw, 0, sizeof(_sw));
-		rc = bpf_map_update_elem(&hmapsw, &key, &_sw, BPF_NOEXIST);
-		if (rc) {
-			if (rc != -EEXIST)
-				goto err;
-		}
-
-		sw = bpf_map_lookup_elem(&hmapsw, &key);
-		/* it must not fail */
-		if (!sw)
-			goto err;
-	}
-
-	update_window(sw, ts);
-	return;
-
-err:
-	bpf_printk("error while creating the slotted_window");
-	return;
-}
-
 SEC("tc")
 int tc_ingress(struct __sk_buff *ctx)
 {
@@ -532,15 +398,295 @@ int tc_ingress(struct __sk_buff *ctx)
 out:
 	/* fill test map used for retrieving hash entries from userland */
 	update_test_hmap(helem);
-
-	/* update slotted window */
-	eval_avg();
-
 	return TC_ACT_OK;
 
 drop:
 	bpf_printk("Dropped IP packet");
 	return TC_ACT_SHOT;
+}
+#endif
+
+#define LOG_SCALE_FACTOR	20
+
+#define DECAY_TABLE_MAX		10
+static const __u64 decay_table[DECAY_TABLE_MAX + 1] = {
+	1048576,
+	635993,
+	385750,
+	233969,
+	141909,
+	86072,
+	52206,
+	31664,
+	19205,
+	11649,
+	7065,
+};
+
+struct swin_key {
+	__u32 id;
+};
+
+#define SWIN_SCALER		1000000000ul /* 1sec in nanosec */
+#define SWIN_TIMER_TIMEOUT	(SWIN_SCALER << 1ul)
+struct slotted_window {
+	/* avoid multiple concurrent window updates */
+	__u64 sync;
+	__u64 init;
+
+	__u64 tsw;
+	__u64 cnt;
+	__u64 avg;
+
+	struct bpf_timer timer;
+};
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1);
+	__type(key, struct swin_key);
+	__type(value, struct slotted_window);
+} hmapsw SEC(".maps");
+
+#define __LOG_SCALE_IN(x)	(((__u64)(x)) << LOG_SCALE_FACTOR)
+#define __LOG_SCALE_OUT(x)	(((__u64)(x)) >> LOG_SCALE_FACTOR)
+
+#define try_swin_lock(sw)	__sync_lock_test_and_set(&(sw)->sync, 1)
+#define swin_unlock(sw)					\
+	do {						\
+		__sync_fetch_and_and(&(sw)->sync, 0);	\
+		barrier();				\
+	} while(0)
+
+static __always_inline
+int update_window_start_timer(struct bpf_timer *timer, __u64 timeout)
+{
+	int rc;
+
+	rc = bpf_timer_start(timer, timeout, 0);
+	if (!rc)
+		return 0;
+
+	if (rc == -EINVAL) {
+		/* This use case can be tolerated, as it is very rare.
+		 * If we have arrived at this point, it indicates that
+		 * two packets are being processed simultaneously on
+		 * two different CPUs, and both are attempting to
+		 * initialize the corresponding element. However, the
+		 * operation is intended to be performed by only one
+		 * CPU.
+		 * Therefore, it is possible that while one CPU is
+		 * initializing the timer, the completed operation may
+		 * not yet be visible on the current CPU.
+		 */
+		bpf_printk("bpf_timer is not initialized yet");
+		return 0;
+	}
+
+	return rc;
+}
+
+static __always_inline
+int update_window(struct slotted_window *sw, __u64 ts, bool start_timer)
+{
+	const __u64 cur_tsw = ts / SWIN_SCALER;
+	__u64 tsw = READ_ONCE(sw->tsw);
+	__u64 *cnt = &sw->cnt;
+	__u64 cnt_val;
+	__u64 delta;
+	__u64 avg;
+	int rc;
+
+	if (cur_tsw <= tsw)
+		goto update;
+
+	if (try_swin_lock(sw))
+		/* busy, another cpu is currently closing the window */
+		goto update;
+
+	/* the current window must be closed */
+	delta = cur_tsw - tsw;
+	if (delta <= DECAY_TABLE_MAX) {
+		if (delta < 1)
+			/* impossibile, uhm? */
+			goto err;
+
+		cnt_val = READ_ONCE(*cnt);
+
+		avg = __LOG_SCALE_OUT((__LOG_SCALE_IN(cnt_val) *
+				      decay_table[(delta - 1)])) +
+			/* sw->avg is only read in this section */
+		      __LOG_SCALE_OUT(sw->avg * decay_table[delta]);
+	} else {
+		avg = 0;
+	}
+
+	WRITE_ONCE(sw->avg, avg);
+
+	/* TODO: send to RING BUFFER rather than print on trace */
+	bpf_printk("CLOSING window; tsw=%llu, avg(fp)=%llu, avg=%llu",
+		   tsw, avg, __LOG_SCALE_OUT(avg));
+
+	/* time to create a new window */
+	WRITE_ONCE(*cnt, 0);
+	WRITE_ONCE(sw->tsw, cur_tsw);
+
+	/* we cannot rely upon the __sync_lock_release() semantic, so we need
+	 * to use a workaround, e.g.: manually set the sw->sync back to 0.
+	 */
+	swin_unlock(sw);
+
+	if (!start_timer)
+		goto update;
+
+	/* start timer bound to this window */
+	rc = update_window_start_timer(&sw->timer, SWIN_TIMER_TIMEOUT);
+	if (rc)
+		goto err;
+
+update:
+	__sync_fetch_and_add(cnt, 1);
+	return 0;
+
+err:
+	return -EINVAL;
+}
+
+static __always_inline int eval_avg(bool start_timer);
+
+static
+int swin_timer_cb(void *map, struct swin_key *key, struct slotted_window *sw)
+{
+	int rc;
+
+	rc = eval_avg(false);
+	if (rc)
+		bpf_printk("swin_timer_cb: eval_avg rc=%d\n", rc);
+
+	return 0;
+}
+
+static __always_inline int swin_timer_init(void *map, struct bpf_timer *timer)
+{
+	int rc;
+
+	rc = bpf_timer_init(timer, map, CLOCK_BOOTTIME);
+	if (rc)
+		return rc;
+
+	return bpf_timer_set_callback(timer, swin_timer_cb);
+}
+
+static __always_inline
+struct slotted_window *slotted_window_init_or_get(void *map,
+						  struct swin_key *key)
+{
+	struct slotted_window *sw, init;
+	int rc;
+
+	sw = bpf_map_lookup_elem(map, key);
+	if (sw)
+		return sw;
+
+	memset(&init, 0, sizeof(init));
+
+	/* note that updating an hashtable element is an atomic op */
+	rc = bpf_map_update_elem(map, key, &init, BPF_NOEXIST);
+	if (rc) {
+		if (rc == -EEXIST)
+			/* another cpu has just createed the entry, give up */
+			goto lookup;
+
+		return NULL;
+	}
+
+lookup:
+	sw = bpf_map_lookup_elem(map, key);
+	if (!sw)
+		return NULL;
+
+	/* see https://reviews.llvm.org/D72184 */
+	if (__sync_lock_test_and_set(&sw->init, 1))
+		/* already initialized */
+		return sw;
+
+	/* only a single CPU can be here */
+
+	/* we want to initialize a timer only once.
+	 * A timer needs to be defined inside a map element which is already
+	 * stored in the map. For this reason, we cannot  use a
+	 * publish/subscribe approach - e.g. create a map element, initialize
+	 * the timer within it and finally update the map with that element.
+	 * Publish allows cpus to see the whole map element fully initialized
+	 * or not.
+	 *
+	 * Our approach in this case, is to allow *only* a  CPU to initialized
+	 * the timer when a new map element is createde and pushed into the
+	 * map. However, in the mean while the CPU is taking the element lock
+	 * and initialize the timer, another CPU could reference to that
+	 * element finding that timer is not still initialized. We admit this
+	 * corner case as it could happen only the first time a new map element
+	 * is created inside the map.
+	 */
+	rc = swin_timer_init(map, &sw->timer);
+	if (rc)
+		return NULL;
+
+	return sw;
+}
+
+static __always_inline int eval_avg(bool start_timer)
+{
+	__u64 ts = bpf_ktime_get_boot_ns();
+	const struct swin_key key = {
+		.id = 0,
+	};
+	struct slotted_window *sw;
+	int rc;
+
+	sw = slotted_window_init_or_get(&hmapsw, (struct swin_key *)&key);
+	if (!sw)
+		return -ENOENT;
+
+	rc = update_window(sw, ts, start_timer);
+	if (rc)
+		return rc;
+
+	return 0;
+}
+
+SEC("tc")
+int tc_ingress(struct __sk_buff *ctx)
+{
+	void *data_end = skb_data(ctx->data_end);
+	void *data = skb_data(ctx->data);
+	struct ethhdr *l2;
+	struct iphdr *l3;
+	int rc;
+
+	if (ctx->protocol != bpf_htons(ETH_P_IP))
+		return TC_ACT_OK;
+
+	l2 = data;
+	if ((void *)(l2 + 1) > data_end)
+		return TC_ACT_OK;
+
+	l3 = (struct iphdr *)(l2 + 1);
+	if ((void *)(l3 + 1) > data_end)
+		return TC_ACT_OK;
+
+	/* here only if we are handling IP packets */
+
+#if 0
+	bpf_printk("! Got IP packet: tot_len: %d, ttl: %d",
+		   bpf_ntohs(l3->tot_len), l3->ttl);
+#endif
+
+	rc = eval_avg(true);
+	if (rc)
+		bpf_printk("error while evaluating avg algo (rc=%d)", rc);
+
+	return TC_ACT_OK;
 }
 
 char __license[] SEC("license") = "GPL";
