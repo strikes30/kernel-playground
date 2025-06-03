cfc920b0286d5b56406b48500744c1229a0698a7
Andrea Mayer <andrea.mayer@uniroma2.it>
2025-06-03 15:07:54 +0000

    Remove sudo from setup scripts to run as root for simplified env setup
    Avoid using sudo in the script since many Linux distributions do not
    support it by default.
    
    To ensure the scripts work correctly, it is preferable to run it
    directly as the root user, especially since security is not a concern in
    our controlled environment.
    
    While using root is generally discouraged and not recommended for
    production systems, in this case, it simplifies setup and allows us to
    install numerous packages and configure privileged containers that can
    access host devices like /dev/kvm without worrying about device
    permissions, ownership, or access control.
    
    Our goal is to create a convenient environment for testing and
    development within a laboratory setting, where the usual security
    considerations are relaxed.
    However, it's important to remember that running as root is discouraged
    in most real-world scenarios due to security risks.
    
    Signed-off-by: Andrea Mayer <andrea.mayer@uniroma2.it>

--- podman/container-build.sh
+++ podman/container-build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+	echo "Error: This script must be run as root." >&2
+	exit 1
+fi
+
 podman build  --no-cache=true  -t kernel-builder .
--- podman/setup-all.sh
+++ podman/setup-all.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-sudo podman run \
+# Check if the script is run as root
+if [ "$EUID" -ne 0 ]; then
+	echo "Error: This script must be run as root." >&2
+	exit 1
+fi
+
+podman run \
 	--privileged \
 	--rm \
 	--replace \
