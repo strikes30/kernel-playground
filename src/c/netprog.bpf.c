a3696ad57f534bcb9f25315ad7dad1f522b35ab3
Andrea Mayer <andrea.mayer@uniroma2.it>
2025-06-03 00:01:33 +0000

    Add sample XDP program to drop IPv6 ICMPv6 packets
    Add a simple XDP program that demonstrates how to drop IPv6 packets when
    the next protocol is ICMPv6.
    To compile the eBPF program, run the command:
      $ make -j$(nproc).
    
    After compilation, copy the resulting .bpf.o file into the VM's shared
    folder:
     $ make install
    
    Signed-off-by: Andrea Mayer <andrea.mayer@uniroma2.it>

--- src/c/netprog.bpf.c
+++ src/c/netprog.bpf.c
@@ -1,13 +1,125 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 #include <vmlinux.h>
+#include <errno.h>
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_tracing.h>
 
+#define ETH_P_IPV6		0x86DD	/* IPv6 */
+#define IPPROTO_ICMPV6		58	/* ICMPv6 */
+
+
+/* Byte-count bounds check; check if current pointer at @start + @off of header
+ * is after @end.
+ */
+#define __may_pull(start, off, end) \
+	(((unsigned char *)(start)) + (off) <= ((unsigned char *)(end)))
+
+/* Header cursor to keep track of current parsing position */
+struct hdr_cursor {
+	void *pos;
+};
+
 SEC("xdp")
-int  xdp_prog_simple(struct xdp_md *ctx)
+int  xdp_prog_pass(struct xdp_md *ctx)
 {
 	return XDP_PASS;
 }
 
-char _license[] SEC("license") = "GPL";
+static __always_inline int
+parse_ethhdr(struct hdr_cursor *nh, void *data_end, struct ethhdr **ethhdr)
+{
+	struct ethhdr *eth = nh->pos;
+	int hdrsize = sizeof(*eth);
+	__u16 h_proto;
+
+	if (!__may_pull(eth, hdrsize, data_end))
+		return -EINVAL;
+
+	/* Move the cursor ahead as we have parsed the ethernet header */
+	nh->pos += hdrsize;
+	/* network-byte-order */
+	h_proto = eth->h_proto;
+
+	if (ethhdr)
+		*ethhdr = eth;
+
+	return h_proto;
+}
+
+static __always_inline int
+parse_ip6hdr(struct hdr_cursor *nh, void *data_end, struct ipv6hdr **ip6hdr)
+{
+	struct ipv6hdr *ip6h = nh->pos;
+	int hdrsize = sizeof(*ip6h);
+
+	/* Pointer-arithmetic bounds check; pointer +1 points to after end of
+	 * thing being pointed to.
+	 */
+	if (!__may_pull(ip6h, hdrsize, data_end))
+		return -EINVAL;
+
+	nh->pos += hdrsize;
+
+	if (ip6hdr)
+		*ip6hdr = ip6h;
+
+	return ip6h->nexthdr;
+}
+
+static __always_inline int
+process_ipv6hdr(struct hdr_cursor *nh, void *data_end)
+{
+	struct ipv6hdr *ip6h;
+	int nexthdr;
+
+	nexthdr = parse_ip6hdr(nh, data_end, &ip6h);
+	if (nexthdr < 0)
+		goto out;
+
+	/* Do processing based on the IPv6 next header. In this specific case,
+	 * drop any ICMPv6 packet.
+	 */
+	if (nexthdr == IPPROTO_ICMPV6) {
+		bpf_printk("XDP: recevied ICMPv6 packet! Drop it");
+		return XDP_DROP;
+	}
+
+out:
+	return XDP_PASS;
+}
+
+SEC("xdp")
+int  xdp_prog_drop_icmpv6(struct xdp_md *ctx)
+{
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data = (void *)(long)ctx->data;
+	struct hdr_cursor nh;
+	struct ethhdr *eth;
+	int h_proto;
+       __u16 proto;
+
+	/* These keep track of the next header type and interator pointer */
+	nh.pos = data;
+
+	h_proto = parse_ethhdr(&nh, data_end, &eth);
+	if (h_proto < 0)
+		/* we cannot parse the ethernet header; instead of droppig the
+		 * packet we allow it to go in the kernel networking stack to
+		 * be further processed.
+		 */
+		goto out;
+
+	/* From network-byte-order to machine endianess */
+	proto = bpf_ntohs(h_proto);
+	switch (proto) {
+	case ETH_P_IPV6:
+		return process_ipv6hdr(&nh, data_end);
+	};
+
+	/* Pass the packet to the upper kernel networking */
+out:
+	return XDP_PASS;
+}
+
+char _license[] SEC("license") = "Dual BSD/GPL";
