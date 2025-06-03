57c10424f4112576b8478cfa583629886d220724
Andrea Mayer <andrea.mayer@uniroma2.it>
2025-06-03 22:49:06 +0000

    add support counting dropped ICMPv6 packets in eBPF/XDP sample prog
    Extend the basic XDP program to include a drop counter for ICMPv6
    packets.
    Each time an ICMPv6 packet is dropped, increment a counter stored in an
    eBPF map. This map can be accessed from user space to track the total
    number of ICMPv6 packets dropped so far.
    
    Signed-off-by: Andrea Mayer <andrea.mayer@uniroma2.it>

--- src/c/netprog.bpf.c
+++ src/c/netprog.bpf.c
@@ -15,6 +15,25 @@
 #define __may_pull(start, off, end) \
 	(((unsigned char *)(start)) + (off) <= ((unsigned char *)(end)))
 
+/* LLVM maps __sync_fetch_and_add() as a built-in function to the BPF atomic add
+ * instruction (that is BPF_STX | BPF_XADD | BPF_W for word sizes)
+ */
+#ifndef lock_xadd
+#define lock_xadd(ptr, val)	((void) __sync_fetch_and_add(ptr, val))
+#endif
+
+struct proc_stats {
+	__u64 drop;
+};
+
+#define XDP_STATS_MAP_NELEM_MAX 1
+struct {
+	__uint(type, BPF_MAP_TYPE_ARRAY);
+	__type(key, __u32);
+	__type(value, struct proc_stats);
+	__uint(max_entries, XDP_STATS_MAP_NELEM_MAX);
+} xdp_stats_map SEC(".maps");
+
 /* Header cursor to keep track of current parsing position */
 struct hdr_cursor {
 	void *pos;
@@ -70,23 +89,41 @@ parse_ip6hdr(struct hdr_cursor *nh, void *data_end, struct ipv6hdr **ip6hdr)
 static __always_inline int
 process_ipv6hdr(struct hdr_cursor *nh, void *data_end)
 {
+	struct proc_stats *pstats;
 	struct ipv6hdr *ip6h;
+	const int key = 0;
 	int nexthdr;
 
 	nexthdr = parse_ip6hdr(nh, data_end, &ip6h);
 	if (nexthdr < 0)
-		goto out;
+		return XDP_PASS;
 
 	/* Do processing based on the IPv6 next header. In this specific case,
 	 * drop any ICMPv6 packet.
 	 */
-	if (nexthdr == IPPROTO_ICMPV6) {
-		bpf_printk("XDP: received ICMPv6 packet! Drop it");
-		return XDP_DROP;
+	if (nexthdr != IPPROTO_ICMPV6)
+		return XDP_PASS;
+
+	/* Lookup in kernel BPF-side return pointer to stats record */
+	pstats = bpf_map_lookup_elem(&xdp_stats_map, &key);
+	if (!pstats) {
+		/* BPF kernel-side verifier will reject program if the
+		 * NULL pointer check isn't performed here. Even-though
+		 * this is a static array where we know key lookup
+		 * XDP_PASS always will succeed.
+		 */
+		bpf_printk("XDP: Cannot access to proc stats, weird!?! Abort!");
+		return XDP_ABORTED;
 	}
 
-out:
-	return XDP_PASS;
+
+	/* Multiple CPUs can access data record. Thus, the accounting needs to
+	 * use an atomic operation.
+	 */
+	lock_xadd(&pstats->drop, 1);
+
+	bpf_printk("XDP: received ICMPv6 packet! Drop it!");
+	return XDP_DROP;
 }
 
 SEC("xdp")
